c951e4886d4fd2a7e506d33085b6ea1d59fb282e
Vikram Acharya <vikramacharya@users.noreply.github.com>
2017-11-12 20:01:30 +0530

    star wars skill

--- Movieguru/alexa-starwars-gyan.py
+++ Movieguru/alexa-starwars-gyan.py
@@ -6,20 +6,21 @@ app = Flask(__name__)
 ask = Ask(app, "/")
 
 
-def getCharacters():
+def getCharacters(character_count):
 	result = requests.get("http://swapi.co/api/people/").json()
 	characters = result.get("results")
-	return map(lambda x: x.get('name'), characters)[:10]
+	return map(lambda x: x.get('name'), characters)[:character_count]
 
 @ask.launch
 def introduction():
 	session.attributes['last_intent'] = 'Launch'
-	return question('I will tell you about the major characters of star wars.')
+	return question('I will tell you about the major characters of star wars. How many characters do you want me to share?')
 
-@ask.intent('ListCharacterIntent')
-def list_charcaters():
-	speech = 'Following are the major character of star wars. '
-	characters = getCharacters()
+@ask.intent('ListCharacterIntent', mapping={'character_count': 'character_count'})
+def list_charcaters(character_count):
+	print(character_count)
+	speech = 'Following are the {} major character of star wars. '.format(character_count)
+	characters = getCharacters(character_count)
 	speech += ','.join(characters)
 	return statement(speech)
 
@@ -27,7 +28,7 @@ def list_charcaters():
 @ask.intent('AMAZON.HelpIntent')
 def get_help():
     if session.attributes.get('last_intent') == 'Launch':
-        return question('I can provide you with the list of characters in the movie Star Wars ')
+        return question('I can provide you with the list of characters in the movie Star Wars. How many characters should I say?')
 
 
 @ask.intent('AMAZON.CancelIntent')
