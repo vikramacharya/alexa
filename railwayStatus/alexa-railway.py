5801875723b1e8e1235f38af52f4dbe07e0a4339
Vikram Acharya <vikramacharya@users.noreply.github.com>
2018-03-04 10:47:17 +0530

    indian railway  skill

--- railwayStatus/alexa-railway.py
+++ railwayStatus/alexa-railway.py
@@ -9,24 +9,70 @@ ask = Ask(app, "/")
 @ask.launch
 def new_game():
     session.attributes['last_intent'] = 'Launch'
-    return question('You can get the latest status of the railway by PNR. Which PNR will it be ?')
-
+    return question('I can provide you with the latest status for any railway PNR you ask me about. '
+                        'Just tell me the PNR of the railway and i will read the status to you. '
+                        'So which pnr do you want to know about ?')
 
 @ask.intent('RailwayNewsIntent', mapping={'pnr': 'pnr'})
 def railway_news_app(pnr):
     try:
-        railway_news_title_list = RailwayNews().main(pnr)
+        ApiResult = RailwayNews().get_pnr_details(pnr)
     except Exception as e:
-        railway_news_title_list = []
+        ApiResult = []
 
-    if not railway_news_title_list:
+    if not ApiResult:
         return statement('Sorry. Could not find any update for %s' % pnr)
     speech = ''
-    for title in railway_news_title_list:
-        title, source = title.rsplit('-', 1)
-        speech += 'According to %s %s.' % (source, title)
-    return statement(speech)
+    
+    #ApiResult = railway.get_pnr_details(6309015112)
+    # print(ApiResult)
+    speech_output = "Welcome to the Alexa Indian Railway PNR Status skill. "
+    doj = ApiResult['doj']
+    pnr = ApiResult['pnr']
+    reservation_upto = ApiResult['reservation_upto']['name']
+    to_station = ApiResult['to_station']['name']
+    train_number = ApiResult['train']['number']
+    train_name = ApiResult['train']['name']
 
+    speech += "Reservation status for "
+    total_passengers = ApiResult['total_passengers']
+    chart_prepared = ApiResult['chart_prepared']
+    response_code = ApiResult['response_code']
+    boarding_point = ApiResult['boarding_point']['name']
+    journey_class = ApiResult['journey_class']['code']
+    passengers = ApiResult['passengers']
+    for x in passengers:
+        booking_status = x['booking_status']
+        current_status = x['current_status']
+        passenger_number = x['no']
+        current_status_value = current_status.split('/')
+        current_status_data = current_status_value[0]
+        if current_status_data == 'CAN':
+            latest_status = 'Cancelled'
+        elif current_status_data == 'CNF':
+            latest_status = 'Confirmed'
+        else:
+            latest_status = 'Chart Not Prepared'
+        speech += "passenger {} is {} with current status {}".format(passenger_number, booking_status, latest_status)
+        if passenger_number != total_passengers:
+            speech += ' and '
+        else:
+            speech += '.'
+
+    speech_output += "PNR " + pnr + " is for train " + train_number + " " + train_name + ". "
+    if chart_prepared:
+        speech_output += "Chart for PNR {} is prepared.".format(pnr)
+    else:
+        speech_output += "Chart for PNR {} is not prepared yet.".format(pnr)
+    speech_output += " Your Boarding point is " + boarding_point + " on " + doj + " and destination is " + reservation_upto + "."
+    speech_output += " You have booked ticker for " + str(total_passengers) + " people. "
+    speech_output += speech
+
+    return statement(speech_output)
+
+speech_output = "Welcome to the Alexa Indian Railway PNR Status Skill. " \
+					"You can ask me for your PNR status for any Indian Railway, " \
+					"and I will provide you with the details on the same."
 
 @ask.intent('AMAZON.HelpIntent')
 def get_help():
@@ -38,12 +84,12 @@ def get_help():
 
 @ask.intent('AMAZON.CancelIntent')
 def cancel_request():
-    return statement('Thank you for using the app. Have a good day')
+    return statement('Thank you for using the Alexa Indian Railway PNR Status Skill. See you next time!')
 
 
 @ask.intent('AMAZON.StopIntent')
 def stop_app():
-    return statement('Thank you for using the app. Have a good day')
+    return statement('Thank you for using the Alexa Indian Railway PNR Status Skill. See you next time!')
 
 
 if __name__ == '__main__':
--- railwayStatus/railway_api.py
+++ railwayStatus/railway_api.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import requests
-
+import json
+from pprint import pprint
 
 class RailwayNews(object):
 
@@ -8,20 +9,65 @@ class RailwayNews(object):
         pass
 
     @staticmethod
-    def get_url(pnr):
-        return 'https://news.google.com/news/section?geo=%s&output=rss' \
-               % ('+'.join(pnr.split()))
+    def get_pnr_details(pnr):
+        url = 'https://api.railwayapi.com/v2/pnr-status/pnr/{}/apikey/{}'.format(pnr, 'uf0ni07b37')
+        response = requests.get(url)
+        return response.json()
+        #result = {u'doj': u'03-02-2017', u'to_station': {u'lat': None, u'lng': None, u'code': u'DBG', u'name': u'DARBHANGA JN'}, u'total_passengers': 2, u'chart_prepared': False, u'passengers': [{u'booking_status': u'CNF/S11/34/GN', u'current_status': u'CNF/-/0/GN', u'no': 1}, {u'booking_status': u'CNF/S11/37/GN', u'current_status': u'CNF/-/0/GN', u'no': 2}], u'response_code': 200, u'pnr': u'8420803198', u'reservation_upto': {u'lat': None, u'lng': None, u'code': u'DBG', u'name': u'DARBHANGA JN'}, u'train': {u'classes': [], u'number': u'11061', u'name': u'DARBHANGA EXP', u'days': []}, u'from_station': {u'lat': None, u'lng': None, u'code': u'LTT', u'name': u'LOKMANYATILAK'}, u'debit': 3, u'journey_class': {u'code': u'SL', u'name': None}, u'boarding_point': {u'lat': None, u'lng': None, u'code': u'LTT', u'name': u'LOKMANYATILAK'}}
+        #result = {u'doj': u'14-02-2017', u'to_station': {u'lat': None, u'lng': None, u'code': u'LTT', u'name': u'LOKMANYATILAK'}, u'total_passengers': 2, u'chart_prepared': False, u'passengers': [{u'booking_status': u'CNF/S3/33/SS', u'current_status': u'CAN/-/0/SS', u'no': 1}, {u'booking_status': u'CNF/S3/36/SS', u'current_status': u'CAN/-/0/SS', u'no': 2}], u'response_code': 200, u'pnr': u'6309015112', u'reservation_upto': {u'lat': None, u'lng': None, u'code': u'LTT', u'name': u'LOKMANYATILAK'}, u'train': {u'classes': [], u'number': u'11062', u'name': u'DBG LTT EXPRESS', u'days': []}, u'from_station': {u'lat': None, u'lng': None, u'code': u'DBG', u'name': u'DARBHANGA JN'}, u'debit': 3, u'journey_class': {u'code': u'SL', u'name': None}, u'boarding_point': {u'lat': None, u'lng': None, u'code': u'DBG', u'name': u'DARBHANGA JN'}}
+        #return result
+    
+# railway = RailwayNews()
+# speech = ''
+# ApiResult = railway.get_pnr_details(6309015112)
+# #print(ApiResult)
+# speech_output = "Welcome to the Alexa Indian Railway PNR Status skill. "
+# doj = ApiResult['doj']
+# pnr = ApiResult['pnr']
+# reservation_upto = ApiResult['reservation_upto']['name']
+# to_station = ApiResult['to_station']['name']
+# train_number = ApiResult['train']['number']
+# train_name = ApiResult['train']['name']
+#
+# speech += "Reservation status for "
+# current_status_speech = "Current status for "
+# total_passengers = ApiResult['total_passengers']
+# chart_prepared = ApiResult['chart_prepared']
+# response_code = ApiResult['response_code']
+# boarding_point = ApiResult['boarding_point']['name']
+# journey_class = ApiResult['journey_class']['code']
+# passengers = ApiResult['passengers']
+# for x in passengers:
+# 	booking_status = x['booking_status']
+# 	current_status = x['current_status']
+# 	passanger_number = x['no']
+# 	current_status_value = current_status.split('/')
+# 	current_status_data = current_status_value[0]
+# 	if current_status_data == 'CAN':
+# 		latest_status = 'Cancelled'
+# 	elif current_status_data == 'CNF':
+# 		latest_status = 'Confirmed'
+# 	else:
+# 		latest_status = 'Chart Not Prepared'
+# 	speech += "passenger {} is {} with current status {}".format(passanger_number, booking_status, latest_status)
+# 	if passanger_number!= total_passengers:
+# 		speech += ' and '
+# 	else:
+# 		speech += '.'
+#
+# speech_output += "PNR "+pnr+" is for train "+train_number+" "+train_name+". "
+# if chart_prepared:
+# 	speech_output += "Chart for PNR {} is prepared.".format(pnr)
+# else:
+# 	speech_output += "Chart for PNR {} is not prepared yet.".format(pnr)
+# speech_output += " Your Boarding point is "+boarding_point+ " on "+ doj+" and destination is "+reservation_upto+"."
+# speech_output += " You have booked ticker for " +str(total_passengers)+ " people. "
+# speech_output += speech
+#
+# print(speech_output)
 
-    @staticmethod
-    def get_rss_xml(url):
-        return requests.get(url).text
+	
 
-    @staticmethod
-    def get_news_title(rss_xml):
-        root = ET.fromstring(rss_xml)
-        return [item.find('title').text for item in root.iter('item')][:5]
 
-    def main(self, place):
-        url = self.get_url(place)
-        rss_xml = self.get_rss_xml(url).encode('utf-8')
-        return self.get_news_title(rss_xml)
+
+
